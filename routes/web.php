80be8cd2ac1e6a46d56baade5c7c2856717bc5a8
TAamaA1 <marselnbho@gmail.com>
2025-03-22 23:51:56 +0700

    profil&edit_profil

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('login', function () {
     return view('login');
@@ -10,4 +11,12 @@ Route::get('register', function () {
 });
 Route::get('forgot_password', function () {
     return view('forgot_password');
+});
+
+Route::get('profil', function () {
+    return view('profil');
+});
+
+Route::get('edit_profil', function () {
+    return view('edit_profil');
 });
