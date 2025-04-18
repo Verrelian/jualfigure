1e43f6741c6f41e0f4820c1833d3d9e06cbac0f6
Verrelian <melanicandra72@gmail.com>
2025-04-18 16:32:35 +0700

    nih punya yudik pweb done

--- resources/views/forgot_password.blade.php
+++ resources/views/forgot_password.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
 
-    <!-- Flowbite + Tailwind -->
+    <!-- Flowbite + Tailwind c -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.tailwindcss.com" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
