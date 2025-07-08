0c5b861291b89ef61b73c01d85f16daa15eea3d1
Verrelian <melanicandra72@gmail.com>
2025-07-08 12:07:38 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\{
     WebController,
     DashboardSellerController,
     SearchController,
-    LeaderboardController
+    LeaderboardController,
     ForgotPasswordController,
     OTPController,
 };
